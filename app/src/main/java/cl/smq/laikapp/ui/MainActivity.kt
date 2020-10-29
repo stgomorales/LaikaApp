ee602b7b3f89c5f2b8c48ad6df9b785a936a5dfe
Santiago Morales <santiago.morales.quilodran@gmail.com>
2020-10-29 01:42:59 -0300

    setup recicler view in BreedListFragment setup observer in BreedListFragment

--- app/src/main/java/cl/smq/laikapp/ui/MainActivity.kt
+++ app/src/main/java/cl/smq/laikapp/ui/MainActivity.kt
@@ -2,18 +2,14 @@ package cl.smq.laikapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import cl.smq.laikapp.R
-import cl.smq.laikapp.ui.main.MainFragment
+import cl.smq.laikapp.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
-        }
+        val binding: MainActivityBinding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
+
--- app/src/main/java/cl/smq/laikapp/ui/dogs/BreedListFragment.kt
+++ app/src/main/java/cl/smq/laikapp/ui/dogs/BreedListFragment.kt
@@ -5,13 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import cl.smq.laikapp.data.entities.DogBreed
 import cl.smq.laikapp.databinding.BreedListFragmentBinding
+import cl.smq.laikapp.ui.adapter.BreedAdapter
+import cl.smq.laikapp.utils.Resource
 
-class BreedListFragment : Fragment() {
+class BreedListFragment : Fragment(), BreedAdapter.BreedItemListener {
 
     private lateinit var binding: BreedListFragmentBinding
     private val viewModel: BreedListViewModel by viewModels()
+    private lateinit var adapter: BreedAdapter
+
 
 
     override fun onCreateView(
@@ -28,11 +37,31 @@ class BreedListFragment : Fragment() {
     }
 
     private fun setupRecyclerView(){
-
+        adapter = BreedAdapter(this)
+        binding.listFragmetRecycler.layoutManager = LinearLayoutManager(requireContext())
+        binding.listFragmetRecycler.adapter = adapter
+        binding.listFragmetRecycler.addItemDecoration(DividerItemDecoration(this.context, DividerItemDecoration.VERTICAL))
     }
 
     private fun setupObservers(){
+        viewModel.dogBreeds.observe(viewLifecycleOwner, Observer {
+            when(it.status){
+                Resource.Status.onSuccess ->{
+                    adapter.setItems(it.data as ArrayList<DogBreed>)
+                    binding.listFragmentProgress.visibility = View.GONE
+                }
+                Resource.Status.onError ->{
+                    Toast.makeText(requireContext(), it.message, Toast.LENGTH_SHORT).show()
+                }
+                Resource.Status.onLoading ->{
+                    binding.listFragmentProgress.visibility = View.GONE
+                }
+            }
+        })
+    }
 
+    override fun onClickedIndicator(breed: String) {
+        TODO("Not yet implemented")
     }
 
 }
