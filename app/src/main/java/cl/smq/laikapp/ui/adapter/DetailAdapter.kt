2ec03dc37644afd2738ac163bb6d2408aef975a8
Santiago Morales <santiago.morales.quilodran@gmail.com>
2020-10-29 02:46:48 -0300

    add detail dog adapter

---
+++ app/src/main/java/cl/smq/laikapp/ui/adapter/DetailAdapter.kt
@@ -0,0 +1,63 @@
+package cl.smq.laikapp.ui.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import cl.smq.laikapp.R
+import cl.smq.laikapp.databinding.BreedDetailRowBinding
+import com.bumptech.glide.Glide
+
+class DetailAdapter (private val listener: DetailItemListener, private val context: Context): RecyclerView.Adapter<DetailViewHolder>(){
+
+
+    interface DetailItemListener {
+        fun onClickedItem(url: String)
+    }
+
+    private lateinit var items: ArrayList<String>
+
+
+    fun setItems(items: ArrayList<String>){
+        this.items.clear()
+        this.items.addAll(items)
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount(): Int = items.size
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DetailViewHolder {
+        val binding: BreedDetailRowBinding = BreedDetailRowBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return DetailViewHolder(binding, listener)
+    }
+
+    override fun onBindViewHolder(holder: DetailViewHolder, position: Int) = holder.bind(items[position], context )
+}
+
+
+class DetailViewHolder(private val detailRowBinding: BreedDetailRowBinding, private  val listener: DetailAdapter.DetailItemListener): RecyclerView.ViewHolder(detailRowBinding.root),
+    View.OnClickListener{
+
+    private lateinit var dogUri: String
+
+    init {
+        detailRowBinding.root.setOnClickListener(this)
+    }
+
+    fun bind(item: String, context: Context){
+        this.dogUri = item
+
+        Glide.with(context)
+            .load(dogUri)
+            .centerCrop()
+            .placeholder(R.drawable.ic_astronaut)
+            .thumbnail(0.5f)
+            .into(detailRowBinding.breedDetailRowIcon);
+    }
+    override fun onClick(p0: View?) {
+        listener.onClickedItem(dogUri)
+    }
+
+}
